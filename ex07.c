a25b61bd7cad0fb5d7c49f7f0d0ebf3f55e6d507
Igor Oliveira <132612240+IgorMLOliveira@users.noreply.github.com>
2023-08-20 14:57:32 -0300

    Checagem de criterios

---
+++ ex07.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// exercicio 7 verificar se os numeros inseridos atendem os creiterios para a soma
+int main(void) {
+  float a, b, c, d;
+  
+  printf("Digite o valor de a: ");
+  scanf("%d",&a);
+  printf("Digite o valor de b: ");
+  scanf("%d",&b);
+  printf("Digite o valor de c: ");
+  scanf("%d",&c);
+  printf("Digite o valor de d: ");
+  scanf("%d",&d);
+  if(a>b && c>d){;
+    if(c+d <a +b);
+      printf("Criterios aceitos");
+      }else{
+      printf("Erro");
+      }
+  return 0;
+}
